60cea203bfa01a9121935b7cca7f642a0cbd94a4
Lance Laughlin <lance.laughlin@cvs.com>
2017-08-15 09:36:18 -0400

    Handling clearning and showing the live search

--- src/components/search_input.js
+++ src/components/search_input.js
@@ -8,14 +8,14 @@ class SearchInput extends Component {
 
 	constructor(props) {
 		super(props);
-		this.state = {term: ''};
+		this.state = {term: '', showLiveSearch: 'true'};
 
 		this.onFormSubmit = this.onFormSubmit.bind(this);
 		this.onInputChange = this.onInputChange.bind(this);
 	}
 
 	onInputChange(event) {
-		this.setState({term: event.target.value});
+		this.setState({'term': event.target.value, 'showLiveSearch': true });
 		if(event.target.value.length > 3) {
 			this.props.fetchFirstMovie(this.state.term);
 		}
@@ -29,18 +29,20 @@ class SearchInput extends Component {
 
 	selectMovie(movie) {
 		this.props.selectFirstMovie(movie.id);
-		this.setState({'term': movie.title});
+		this.setState({'term': movie.title, 'showLiveSearch': false});
 	}
 
 	renderLiveSearch() {
 		console.log("props movie: ", this.props.movies);
-		return this.props.movies.map((movie) => {
-			return(
-				<li key={movie.id} className="list-group-item">
-					<button type="button" onClick={() => this.selectMovie(movie)}>{movie.title}</button>
-				</li>
-			)
-		});
+		if(this.state.showLiveSearch){
+			return this.props.movies.map((movie) => {
+				return(
+					<li key={movie.id} className="list-group-item">
+						<button type="button" onClick={() => this.selectMovie(movie)}>{movie.title}</button>
+					</li>
+				)
+			});
+		}
 	}
 
 	render() {
--- src/reducers/reducer_selected_movie.js
+++ src/reducers/reducer_selected_movie.js
@@ -3,8 +3,6 @@ import { SELECT_FIRST_MOVIE } from '../actions/index';
 export default function(state=[], action){
 	console.log('action: ', action);
 
-
-
 	switch(action.type) {
 		case SELECT_FIRST_MOVIE:
 			return action.payload;
